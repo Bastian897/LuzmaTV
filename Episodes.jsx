af1315075caad7a2067cd561f09c46d223cc3b15
Bastian Moreno <Morenobastian897@gmail.com>
2026-07-15 09:43:23 -0400

    fix: corrige link "Ver todos" en episodios mezclados a Comunidad AM

--- Episodes.jsx
+++ Episodes.jsx
@@ -7,7 +7,7 @@ function Episodes() {
       title="Últimos episodios"
       background="#fff"
       action={
-        <Button variant="ghost" size="sm" as="a" href="/programa/luzma-cachai" onClick={lzmNavClick('/programa/luzma-cachai')}>
+        <Button variant="ghost" size="sm" as="a" href="/programa/comunidad-am" onClick={lzmNavClick('/programa/comunidad-am')}>
           Ver todos <Icon name="chevronRight" size={14} />
         </Button>
       }
